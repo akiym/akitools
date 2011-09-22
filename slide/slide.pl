ccc656349ce5a8ce06f533aa8f02d2482260e381
akiym <akiyama@8ant.org>
2011-09-22 22:57:16 +0900

    minor fixes

--- slide/slide.pl
+++ slide/slide.pl
@@ -5,23 +5,27 @@ use Text::MultiMarkdown qw/markdown/;
 use Data::Section::Simple qw/get_data_section/;
 use Pod::Usage;
 
-my $file = shift or pod2usage(1);
+my $file = shift or pod2usage(0);
 
 my $content = do {
     open my $fh, '<', $file or die $!;
-    local $/; <$fh>;
+    local $/; <$fh>
 };
 
 my @slides = map { markdown($_) } split /\n\n\n/, $content;
 my ($title) = $slides[0] =~ m!<h1\s*[\w="]*>(.*?)</h1>!;
 
-my $tx = Text::Xslate->new();
+my $tx = Text::Xslate->new;
 print $tx->render_string(get_data_section('slide.tx'), {
-    title => $title,
+    title  => $title,
     slides => \@slides,
 });
 
 __END__
+
+=head1 NAME
+
+slide.pl - Slide generator written in Markdown
 
 =head1 SYNOPSIS
 
@@ -37,12 +41,10 @@ __DATA__
 <head>
     <meta charset="utf-8" />
     <title><: $title :></title>
-    <link rel="stylesheet" href="static/css/screen.css" type="text/css" media="screen, projection" />
-    <link rel="stylesheet" href="static/css/prettify.css" type="text/css" />
-    <link rel="stylesheet" href="static/css/slide.css" type="text/css" />
-    <script type="text/javascript" src="static/js/jquery-1.6.2.min.js"></script>
-    <script type="text/javascript" src="static/js/jquery.presentation.js"></script>
-    <script type="text/javascript" src="static/js/prettify/prettify.js"></script>
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="../static/css/slide.css" type="text/css" />
+    <script type="text/javascript" src="../static/js/jquery-1.6.2.min.js"></script>
+    <script type="text/javascript" src="../static/js/jquery.presentation.js"></script>
 </head>
 <body>
     <div id="slides">
@@ -53,11 +55,6 @@ __DATA__
     </div>
     <script>
 $(function () {
-    $(document).ready(function () {
-        $('pre').addClass('prettyprint');
-        prettyPrint();
-    });
-
     $('#slides').presentation();
 });
     </script>
